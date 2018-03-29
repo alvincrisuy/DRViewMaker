a8073d55e66dd81a2481be264708ea876b0b3780
Dejan Ribnikar <dejanribnikar@gmail.com>
2018-03-29 11:14:16 +0300

    0.1.1

--- DRViewMaker/DRViewMaker.swift
+++ DRViewMaker/DRViewMaker.swift
@@ -11,9 +11,13 @@ import UIKit
 public class DRViewMaker {
     private init() {}
     
-    public static func makeLabel() -> UILabel {
+    public static func makeLabel(adjustsFontSizeToFitWidth: Bool, font: UIFont, textAlignment: NSTextAlignment, numberOfLines: Int) -> UILabel {
         let label: UILabel = {
             let label = UILabel()
+            label.numberOfLines = numberOfLines
+            label.textAlignment = textAlignment
+            label.font = font
+            label.adjustsFontSizeToFitWidth = adjustsFontSizeToFitWidth
             label.translatesAutoresizingMaskIntoConstraints = false
             return label
         }()
@@ -46,6 +50,7 @@ public class DRViewMaker {
     public static func makeImageViewWithCornerRadius(image: UIImage?, cornerRadius: CGFloat) -> UIImageView {
         let imageView: UIImageView = {
             let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFill
             imageView.layer.cornerRadius = cornerRadius
             imageView.layer.masksToBounds = true
             imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -57,6 +62,7 @@ public class DRViewMaker {
     public static func makeImageViewWithGestureRecognizer(image: UIImage?, gestureRecognizer: UIGestureRecognizer) -> UIImageView {
         let imageView: UIImageView = {
             let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFill
             imageView.isUserInteractionEnabled = true
             imageView.addGestureRecognizer(gestureRecognizer)
             imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -68,6 +74,7 @@ public class DRViewMaker {
     public static func makeImageViewWithImage(image: UIImage?) -> UIImageView {
         let imageView: UIImageView = {
             let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFill
             imageView.translatesAutoresizingMaskIntoConstraints = false
             return imageView
         }()
@@ -110,5 +117,63 @@ public class DRViewMaker {
             return cv
         }()
         return collectionView
+    }
+    
+    public static func makeTextView(delegate: UITextViewDelegate, font: UIFont) -> UITextView {
+        let textView: UITextView = {
+            let tv = UITextView()
+            tv.font = font
+            tv.delegate = delegate
+            tv.translatesAutoresizingMaskIntoConstraints = false
+            return tv
+        }()
+        return textView
+    }
+    
+    public static func make(delegate: UISearchBarDelegate, searchBarStyle: UISearchBarStyle, keyboardType: UIKeyboardType, placeholder: String?) -> UISearchBar {
+        let searchBar: UISearchBar = {
+            let search = UISearchBar()
+            search.searchBarStyle = searchBarStyle
+            search.delegate = delegate
+            search.placeholder = placeholder
+            search.keyboardType = keyboardType
+            search.translatesAutoresizingMaskIntoConstraints = false
+            return search
+        }()
+        return searchBar
+    }
+    
+    public static func makeSegmentedControl(items: [Any], selectedSegmentIndex: Int, tintColor: UIColor?) -> UISegmentedControl {
+        let segmentedControl: UISegmentedControl = {
+            let sc = UISegmentedControl(items: items)
+            sc.tintColor = tintColor
+            sc.selectedSegmentIndex = selectedSegmentIndex
+            sc.translatesAutoresizingMaskIntoConstraints = false
+            return sc
+        }()
+        return segmentedControl
+    }
+    
+    public static func makeSlider(minimumValue: Float, maximumValue: Float, minimumValueImage: UIImage?, maximumValueImage: UIImage?) -> UISlider {
+        let slider: UISlider = {
+            let slider = UISlider()
+            slider.minimumValue = minimumValue
+            slider.minimumValueImage = minimumValueImage
+            slider.maximumValue = maximumValue
+            slider.maximumValueImage = maximumValueImage
+            slider.translatesAutoresizingMaskIntoConstraints = false
+            return slider
+        }()
+        return slider
+    }
+    
+    public static func makeSwitch(isOn: Bool) -> UISwitch {
+        let mySwitch: UISwitch = {
+            let myswitch = UISwitch()
+            myswitch.isOn = isOn
+            myswitch.translatesAutoresizingMaskIntoConstraints = false
+            return myswitch
+        }()
+        return mySwitch
     }
 }
